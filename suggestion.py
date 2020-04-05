3923a307c2c5ebbac600356a9f70da40b117bc30
manipular <9205680+manipular@users.noreply.github.com>
2020-04-05 18:39:08 +0300

    Update suggestion.py

--- suggestion.py
+++ suggestion.py
@@ -4,10 +4,12 @@ import random
 #CSV READ & GENRE-TITLE
 data = pd.read_csv("data.csv")
 
+#CHOICE INPUT
 choice = input('Please enter a word = ')
 
+#SUGGESTION LOOP
 while choice != "exit":
-    choice = choice.lower()gu
+    choice = choice.lower()
     for index, row in data.iterrows():
         if choice in row['genre'].lower():
             result = [row['title']]
